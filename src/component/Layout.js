5ae1fae39ff6e0270439eb75992f0ec31bb4ed3f
webdenway <clthbbd@gmail.com>
2020-08-29 22:52:18 +0600

    final update

--- src/component/Layout.js
+++ src/component/Layout.js
@@ -12,9 +12,9 @@ const Layout = (props) => {
                                                 
                   <div className={"card  m-1"}>
                     <div className={"card-body"}>
-                    <h5 className={"card-title"}>Course Name{name}</h5>
-                    <h6 className={"card-subtitle mb-2 text-muted"}>${price}</h6>
-                    <p className={"card-text"}>{instractor}</p>
+                    <h5 className={"card-title"}>Course Name: {name}</h5>
+                    <h6 className={"card-subtitle mb-2 text-muted"}>Price : ${price}</h6>
+                    <p className={"card-text"}>Instructor : {instractor}</p>
                     <button className={"btn btn-success"} onClick={()=>props.buttonHandelar(props.enrollCourse)}>Enroll this course</button>
                     
                     </div>
